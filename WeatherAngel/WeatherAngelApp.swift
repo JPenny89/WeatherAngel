58807bfd74d87ecc977cf4e8f4b213c872405c9a
James Penny <james_penny@hotmail.co.uk>
2023-07-16 15:43:10 +0100

    Initial Commit

---
+++ WeatherAngel/WeatherAngelApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAngelApp.swift
+//  WeatherAngel
+//
+//  Created by James Penny on 16/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAngelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
